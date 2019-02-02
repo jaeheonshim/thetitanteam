97fd52d7f4a2ccb49d7035880bcab302c538a900
Jaeheon Shim <jaeheonshim@learnthetechnology.com>
2019-02-02 11:14:42 -0500

    Update thecode.js

--- thecode.js
+++ thecode.js
@@ -1,5 +1,11 @@
+/*
+thetitanteam.com - Information about your school day
+By Jaeheon Shim
+Javascript
+*/
 function main() {
 	var day = 86400;
+	var twohour = true;
 	var splittime, hours, minutes, seconds, hourselapsed, minuteselapsed, secondselapsed, absoluteTime, timeformat;
 	setInterval(updates, 1000);
 
@@ -14,6 +20,7 @@ function main() {
 	function preferredTime() {
 		timeformat = document.getElementById("preferredtime").value;
 	}
+
 	function updates() {
 		preferredTime();
 		updateMainTime();
@@ -27,9 +34,11 @@ function main() {
 		seconds = parseInt(splittime[2]);
 		absoluteTime = hoursToSeconds(hours) + minutesToSeconds(minutes) + seconds;
 	}
+
 	function minTwoDigits(n) {
 		return (n < 9 ? '0' : '') + n;
 	}
+
 	function updateMainTime() {
 		if(timeformat == "24") {
 			document.getElementById("current-time").innerHTML = time();
@@ -48,12 +57,31 @@ function main() {
 		/* 
 		Information about time in seconds:
 		28200: 7:50 AM (The time when school starts)
-		54300: 3:05 PM (The time when school ends
+		54300: 3:05 PM (The time when school ends)
+
+		------Two Hour Delay------
+		35400: 9:50 AM (School start time for two hour delay schedule)
+		School still ends at the same time
 		*/
-		if((absoluteTime < 28200) || (absoluteTime > 54300)) {
-			document.getElementById("time-elapsed").innerHTML = "It is outside of school hours";
+		if(twohour == true) { //if current schedule is on two hour delay
+			if((absoluteTime < 35400) || (absoluteTime > 54300)) {
+				document.getElementById("time-elapsed").innerHTML = "It is outside of school hours";
+			}
+			else {
+				var totalSeconds = (hoursToSeconds(hours) + minutesToSeconds(minutes) + seconds) - 35399; //The total amount of time in seconds that has passed MINUS the 7 hours 50 minutes you were not at school.
+				secondselapsed = totalSeconds;
+				hours = Math.floor(totalSeconds / 3600);
+				totalSeconds %= 3600;
+				minutes = Math.floor(totalSeconds / 60);
+				seconds = Math.floor(totalSeconds % 60);
+				document.getElementById("time-elapsed").innerHTML = hours + ":" + minTwoDigits(minutes) + ":" + minTwoDigits(seconds);
+			}
 		}
-		else{
+		else {
+			if((absoluteTime < 28200) || (absoluteTime > 54300)) {
+				document.getElementById("time-elapsed").innerHTML = "It is outside of school hours";
+			}
+			else {
 			var totalSeconds = (hoursToSeconds(hours) + minutesToSeconds(minutes) + seconds) - 28199; //The total amount of time in seconds that has passed MINUS the 7 hours 50 minutes you were not at school.
 			secondselapsed = totalSeconds;
 			hours = Math.floor(totalSeconds / 3600);
@@ -63,6 +91,8 @@ function main() {
 			document.getElementById("time-elapsed").innerHTML = hours + ":" + minTwoDigits(minutes) + ":" + minTwoDigits(seconds);
 		}
 	}
+}
+
 	function updateTimeLeft() {
 		if((absoluteTime < 28200) || (absoluteTime > 54300)) {
 			document.getElementById("time-left").innerHTML = "It is outside of school hours";
@@ -76,8 +106,35 @@ function main() {
 			document.getElementById("time-left").innerHTML = hoursleft + ":" + minTwoDigits(minutesleft) + ":" + minTwoDigits(secondsleft);
 		}
 	}
-	function getPeriod() {
-		console.log(secondselapsed);
+
+function getPeriod() {
+	/* 
+	Timetable for Titan team
+	Period 1: 7:50 AM - 8:40 AM
+	Period 2: 8:45 AM - 9:30 AM
+	Period 3: 9:35 AM - 10:20 AM
+	Period 4: 10:25 AM - 11:10 AM
+	Period 5: 11:15 AM - 11:45 AM
+	Period 6: 11:50 AM - 12:35 PM
+	Period 7: 12:40 PM - 1:25 PM
+	Period 8: 1:30 PM - 2:15 PM
+	Period 9: 2:20 PM - 3:05 PM
+
+	=============================
+
+	TWO HOUR DELAY SCHEDULE:
+	Period 1: 9:50 AM - 10:25 AM
+	Period 2: 10:30 AM - 11:00 AM
+	Period 3: 11:05 AM - 11:35 AM
+	Period 5: 11:40 AM - 12:10 PM
+	Period 6: 12:15 PM - 12:45 PM
+	Period 7: 12:50 PM - 1:20 PM
+	Period 4: 1:25 PM - 1:55 PM
+	Period 8: 2:00 PM - 2:30 PM
+	Period 9: 2:35 PM - 3:05 PM
+	*/
+	console.log(secondselapsed);
+	if(twohour == false) {
 		if (secondselapsed < 3000) {
 			//period 1 is 5 minutes longer than the other periods to allow for the morning announcements.
 			document.getElementById("current-class").innerHTML = "Period 1";
@@ -131,8 +188,64 @@ function main() {
 			document.getElementById("current-class").innerHTML = "Period 9 (Exploratory)";
 		}
 	}
-	function getTimeLeftInPeriod() {
-		if (secondselapsed < 3000) {
+	else {
+		if (secondselapsed < 2100) {
+			document.getElementById("current-class").innerHTML = "Period 1";
+		}
+		if (secondselapsed > 2100 && secondselapsed < 2400) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 2400 && secondselapsed < 4200) {
+			document.getElementById("current-class").innerHTML = "Period 2";
+		}
+		if (secondselapsed > 4200 && secondselapsed < 4500) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 4500 && secondselapsed < 6300) {
+			document.getElementById("current-class").innerHTML = "Period 3";
+		}
+		if (secondselapsed > 6300 && secondselapsed < 6600) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 6600 && secondselapsed < 8400) {
+			document.getElementById("current-class").innerHTML = "Period 5 (Lunch)";
+		}
+		if (secondselapsed > 8400 && secondselapsed < 8700) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 8700 && secondselapsed < 10500) {
+			document.getElementById("current-class").innerHTML = "Period 6";
+		}
+		if (secondselapsed > 10500 && secondselapsed < 10800) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 10800 && secondselapsed < 12600) {
+			document.getElementById("current-class").innerHTML = "Period 7";
+		}
+		if (secondselapsed > 12600 && secondselapsed < 12900) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 12900 && secondselapsed < 14700) {
+			document.getElementById("current-class").innerHTML = "Period 4";
+		}
+		if (secondselapsed > 14700 && secondselapsed < 15000) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 15000 && secondselapsed < 16800) {
+			document.getElementById("current-class").innerHTML = "Period 8 (Exploratory)";
+		}
+		if (secondselapsed > 16800 && secondselapsed < 17100) {
+			document.getElementById("current-class").innerHTML = "Passing Period";
+		}
+		if (secondselapsed > 17100 && secondselapsed < 18900) {
+			document.getElementById("current-class").innerHTML = "Period 9 (Exploratory)";
+		}
+	}
+}
+
+function getTimeLeftInPeriod() {
+	if(twohour == false) {
+	if (secondselapsed < 3000) {
 			return 3000 - secondselapsed; //Period 1
 		}
 		if (secondselapsed > 3000 && secondselapsed < 3300) {
@@ -184,6 +297,61 @@ function main() {
 			return 26100 - secondselapsed; //period 9
 		}
 	}
+	else{
+		//two hour delay
+		if (secondselapsed < 2100) {
+			return 2100 - secondselapsed;
+		}
+		if (secondselapsed > 2100 && secondselapsed < 2400) {
+			return 2400 - secondselapsed;
+		}
+		if (secondselapsed > 2400 && secondselapsed < 4200) {
+			return 4200 - secondselapsed;
+		}
+		if (secondselapsed > 4200 && secondselapsed < 4500) {
+			return 4500 - secondselapsed;
+		}
+		if (secondselapsed > 4500 && secondselapsed < 6300) {
+			return 6300 - secondselapsed;
+		}
+		if (secondselapsed > 6300 && secondselapsed < 6600) {
+			return 6600 - secondselapsed;
+		}
+		if (secondselapsed > 6600 && secondselapsed < 8400) {
+			return 8400 - secondselapsed;
+		}
+		if (secondselapsed > 8400 && secondselapsed < 8700) {
+			return 8700 - secondselapsed;
+		}
+		if (secondselapsed > 8700 && secondselapsed < 10500) {
+			return 10500 - secondselapsed;
+		}
+		if (secondselapsed > 10500 && secondselapsed < 10800) {
+			return 10800 - secondselapsed;
+		}
+		if (secondselapsed > 10800 && secondselapsed < 12600) {
+			return 12600 - secondselapsed;
+		}
+		if (secondselapsed > 12600 && secondselapsed < 12900) {
+			return 12900 - secondselapsed;
+		}
+		if (secondselapsed > 12900 && secondselapsed < 14700) {
+			return 14700 - secondselapsed;
+		}
+		if (secondselapsed > 14700 && secondselapsed < 15000) {
+			return 15000 - secondselapsed;
+		}
+		if (secondselapsed > 15000 && secondselapsed < 16800) {
+			return 16800 - secondselapsed;
+		}
+		if (secondselapsed > 16800 && secondselapsed < 17100) {
+			return 17100 - secondselapsed;
+		}
+		if (secondselapsed > 17100 && secondselapsed < 18900) {
+			return 18900 - secondselapsed;
+		}
+	}
+}
 	function setTimeLeftInPeriod() {
 		var totalSecondsLeft = getTimeLeftInPeriod();
 		console.log(totalSecondsLeft);
@@ -194,10 +362,10 @@ function main() {
 		document.getElementById("current-period-left").innerHTML = hoursleft + ":" + minTwoDigits(minutesleft) + ":" + minTwoDigits(secondsleft);
 		document.title = "Left in period:" + hoursleft + ":" + minTwoDigits(minutesleft) + ":" + minTwoDigits(secondsleft);
 	}
-}
-function time() {
-	var date = new Date();
-	var split = String(date).split(" ");
-	var time = split[4];
-	return time;
+	function time() {
+		var date = new Date();
+		var split = String(date).split(" ");
+		var time = split[4];
+		return time;
+	}
 }
