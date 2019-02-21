7874a02d28bdc32f5349d3f7d8e0faa85636a17a
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2019-02-21 12:15:20 -0500

    Update thecode.js

--- js/thecode.js
+++ js/thecode.js
@@ -4,7 +4,7 @@ By Jaeheon Shim
 Javascript
 */
 var day = 86400;
-var twohour = neither; //idk what is happening today
+var twohour = false;
 var splittime, hours, minutes, seconds, hourselapsed, minuteselapsed, secondselapsed, absoluteTime, timeformat;
 function main() {
 	setInterval(updates, 1000);
