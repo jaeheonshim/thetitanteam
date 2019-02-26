00e5af057398f5a1b91e86b63329d6636c43b774
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2019-02-26 08:36:43 -0500

    Update frontendupdates.js

--- js/frontendupdates.js
+++ js/frontendupdates.js
@@ -110,4 +110,11 @@ function loadChannelT(name) {
 	});
 }
 
-function openNav() { document.getElementById("sidebarnav").style.width = "250px"; document.getElementById("main").style.marginLeft = "250px"; } /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */ function closeNav() { document.getElementById("sidebarnav").style.width = "0"; document.getElementById("main").style.marginLeft = "0"; }
+function openNav() { 
+	document.getElementById("sidebarnav").style.width = "250px"; 
+	document.getElementById("main").style.marginLeft = "250px"; 
+} 
+function closeNav() { 
+	document.getElementById("sidebarnav").style.width = "0"; 
+	document.getElementById("main").style.marginLeft = "0";
+}
